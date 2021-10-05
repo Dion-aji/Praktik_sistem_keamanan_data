a4ead54012c8db163ddd08a0d0635e5c8bf108e3
Dion-aji <89619671+Dion-aji@users.noreply.github.com>
2021-10-05 21:49:04 +0700

    Update Enkripsi.java

--- Playfair_Cipher/PlayFair Cipher_TI D_V3920018_Dion Aji Cahyono/Source Code/Enkripsi.java
+++ Playfair_Cipher/PlayFair Cipher_TI D_V3920018_Dion Aji Cahyono/Source Code/Enkripsi.java
@@ -1,12 +1,12 @@
-public class Enkripsi extends playfair{
+public class Enkripsi extends playfair{// class Enkripsi akan mengextend ke class playFair
 
     public static void main(String[] args) {
         playfair playFair= new playfair();
-        String x=playFair.enkripsi("dionajicahyono");
+        String x=playFair.enkripsi("dionajicahyono");// Data yang ingin di enkripsi ,berikut adalah nama saya
         System.out.println("Kunci : Madiun ");
         System.out.println("Plain Text : dionajicahyono ");
         System.out.println("----------------------------");
-        System.out.println("Enkripsi : "+x);
+        System.out.println("Enkripsi : "+x);//Enkripsi akan memproses dari data x 
         System.out.println("Dekripsi  : "+playFair.dekripsi(x));
     }
 }
